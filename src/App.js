04ae68354cad08a8e69edc89075c76796ae0fcc4
Schleeh <marco-dominik.schleeh@capgemini.com>
2021-08-15 21:08:48 +0200

    s3image to app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 import Tabs from "./components/Tabs"; 
 import Amplify, { API, Storage } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { withAuthenticator, S3Image } from 'aws-amplify-react-native';
+import {  StyleSheet,  Text,  Button,  View,  Image} from 'react-native';
 
 Amplify.configure(awsconfig)
+
+async getFile() {
+    let name = 'public/_2021-5-10-85354_01a0c537-9269-4bf1-9ef4-4c081b4fe3f0.jpg';
+    let fileUrl = await Storage.get(name);
+    this.setState({
+      url: fileUrl
+    })
+  }
 
 function App() { 
  return (
@@ -13,7 +21,21 @@ function App() {
       <h1>Production Cell</h1>
 		<Tabs>
 			<div label="Quality Results">
-				<S3Image imgKey={public/_2021-5-10-85354_01a0c537-9269-4bf1-9ef4-4c081b4fe3f0.jpg} />
+				<View style={styles.container}>
+					<Text>Storage</Text>
+					<Button
+						title="Get Image"
+						onPress={this.getFile.bind(this)}
+					/>
+					{
+						this.state.url !== '' && (
+							<Image
+								source={{ uri: this.state.url }}
+								style={{ width: 300, height: 300 }}
+							/>
+						)
+					}
+      </View>
 			</div>
 			<div label="Logistic Results">
 				No results yet
